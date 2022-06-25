426a9cb2b05fb055bd8668655fe3540219e62e0c
Novelier-Webbelier <novelier.webbelier@gmail.com>
2022-06-26 00:44:29 +0900

    6.10 Cross Board Movement

--- src/App.tsx
+++ src/App.tsx
@@ -26,15 +26,33 @@ function App() {
 
   const onDragEnd = (info: DropResult) => {
     const { destination, draggableId, source } = info;
-    if (destination?.droppableId === source.droppableId) {
+
+    if (!destination) return;
+
+    if (destination.droppableId === source.droppableId) {
       setToDos((allBoards) => {
-        console.log(allBoards);
         const boardCopy = [...allBoards[source.droppableId]];
         boardCopy.splice(source.index, 1);
-        boardCopy.splice(destination?.index, 0, draggableId);
+        boardCopy.splice(destination.index, 0, draggableId);
         return {
           ...allBoards,
           [source.droppableId]: boardCopy,
+        };
+      });
+    }
+
+    if (destination.droppableId !== source.droppableId) {
+      setToDos((allBoards) => {
+        const sourceBoard = [...allBoards[source.droppableId]];
+        const destinationBoard = [...allBoards[destination.droppableId]];
+
+        sourceBoard.splice(source.index, 1);
+        destinationBoard.splice(destination.index, 0, draggableId);
+
+        return {
+          ...allBoards,
+          [source.droppableId]: sourceBoard,
+          [destination.droppableId]: destinationBoard,
         };
       });
     }
