fe4f416a69cd81ae9a0e669483aac9e41caf3236
Novelier-Webbelier <novelier.webbelier@gmail.com>
2022-06-26 19:04:17 +0900

    6.12 Final Styles

--- src/Components/DraggableCard.tsx
+++ src/Components/DraggableCard.tsx
@@ -2,12 +2,19 @@ import React from "react";
 import { Draggable } from "react-beautiful-dnd";
 import styled from "styled-components";
 
-const Card = styled.div`
+const Card = styled.div<ICardProps>`
   border-radius: 5px;
   padding: 10px 10px;
-  background-color: ${(props) => props.theme.cardColor};
   margin-bottom: 10px;
+  box-shadow: ${(props) =>
+    props.isDragging ? "2px 2px 10px rgba(0, 0, 0, 0.5)" : "none"};
+  background-color: ${(props) =>
+    props.isDragging ? "#71b9ff" : props.theme.cardColor};
 `;
+
+interface ICardProps {
+  isDragging: boolean;
+}
 
 interface IDraggableCardProps {
   toDo: string;
@@ -17,11 +24,12 @@ interface IDraggableCardProps {
 function DraggableCard({ toDo, index }: IDraggableCardProps) {
   return (
     <Draggable draggableId={toDo} index={index} key={index}>
-      {(magic) => (
+      {(magic, info) => (
         <Card
           ref={magic.innerRef}
           {...magic.draggableProps}
           {...magic.dragHandleProps}
+          isDragging={info.isDragging}
         >
           <span>{toDo}</span>
         </Card>
